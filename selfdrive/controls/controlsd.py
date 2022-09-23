b383b7a94828659984b8e4d865a86a45bc2c71b3
sunnyc-op <95874127+sunnyc-op@users.noreply.github.com>
2022-09-22 19:24:11 -0700

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -875,7 +875,7 @@ class Controls:
 
     controlsState.steerRatio = float(self.steerRatio_to_send)
     #controlsState.steerActuatorDelay = float(Decimal(Params().get("SteerActuatorDelayAdj", encoding="utf8")) * Decimal('0.01'))
-    controlsState.steerActuatorDelay = ntune_common_get('steerActuatorDelay') if params.get_bool("UseNpilotManager") else float(Decimal(Params().get("SteerActuatorDelayAdj", encoding="utf8")) * Decimal('0.01'))
+    controlsState.steerActuatorDelay = ntune_common_get('steerActuatorDelay') if Params().get_bool("UseNpilotManager") else float(Decimal(Params().get("SteerActuatorDelayAdj", encoding="utf8")) * Decimal('0.01'))
 
     controlsState.sccGasFactor = ntune_scc_get('sccGasFactor')
     controlsState.sccBrakeFactor = ntune_scc_get('sccBrakeFactor')
