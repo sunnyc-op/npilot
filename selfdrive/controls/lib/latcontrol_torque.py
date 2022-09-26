5f5598c3cf110c4ec5d5dd70f1a8bb917311abc3
Comma Device <device@comma.ai>
2022-09-26 21:30:43 +0000

    Update nPilot Manager

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -61,7 +61,10 @@ class LatControlTorque(LatControl):
       actual_lateral_accel = actual_curvature * CS.vEgo ** 2
       lateral_accel_deadzone = curvature_deadzone * CS.vEgo ** 2
 
-      isLowSpeed  = ntune_torque_get('isLowSpeedFactor') if Params().get_bool('UseNpilotManager') else Params().get_bool('IsLowSpeedFactor')
+      try:
+        isLowSpeed  = ntune_torque_get('isLowSpeedFactor') if Params().get_bool('UseNpilotManager') else Params().get_bool('IsLowSpeedFactor')
+      except:
+        isLowSpeed  = Params().get_bool('IsLowSpeedFactor')
 
       if isLowSpeed:
         #low_speed_factor = interp(CS.vEgo, [0, 10, 20], [100, 75, 75])
