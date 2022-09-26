e93ed4ecd46b91bbb07be974bd586fe4a193a6ca
janpoo6427 <janpoo6427@gmail.com>
2022-09-26 16:12:57 +0900

    for tucson

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -176,7 +176,7 @@ class CarInterfaceBase(ABC):
       if cs_out.seatbeltUnlatched:
         events.add(EventName.seatbeltNotLatched)
     if cs_out.gearShifter != GearShifter.drive and (extra_gears is None or
-       cs_out.gearShifter not in extra_gears):
+       cs_out.gearShifter not in extra_gears) and not cs_out.brakePressed:
       events.add(EventName.wrongGear)
     if cs_out.gearShifter == GearShifter.reverse:
       events.add(EventName.reverseGear)
