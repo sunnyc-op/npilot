8ce86a9f189ad2283ce19f861184f60f9ad27ef0
sunnyc-op <sunny68@outlook.com>
2022-09-28 08:20:03 -0700

    Update Camera offset

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -15,7 +15,7 @@ from selfdrive.controls.lib.latcontrol_torque import LatControlTorque
 from selfdrive.swaglog import cloudlog
 from selfdrive.boardd.boardd import can_list_to_can_capnp
 from selfdrive.car.car_helpers import get_car, get_startup_event, get_one_can
-from selfdrive.controls.lib.lane_planner import CAMERA_OFFSET, CAMERA_OFFSET_A
+from selfdrive.controls.lib.lane_planner import CAMERA_OFFSET
 from selfdrive.controls.lib.drive_helpers import update_v_cruise, initialize_v_cruise
 from selfdrive.controls.lib.drive_helpers import get_lag_adjusted_curvature
 from selfdrive.controls.lib.longcontrol import LongControl
