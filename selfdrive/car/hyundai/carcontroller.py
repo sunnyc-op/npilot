332047fd00761567bb84ef167312d68c88492b9c
sunnyc-op <sunny68@outlook.com>
2022-10-31 11:17:48 -0700

    Stopline Test Version.

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -300,19 +300,18 @@ class CarController:
                 if not CS.out.cruiseState.standstill:
                   if stop_distance < 2.0:
                     apply_accel = self.accel - (DT_CTRL * 5.0)
-                  #elif aReqValue < 0.0 and stop_distance < self.stoppingdist  and apply_accel >= aReqValue:
                   elif stop_distance < self.stoppingdist:
                     apply_accel = self.accel - (DT_CTRL * interp(CS.out.vEgo, [0.5, 2.0], [1.0, 5.0]))  
-                  # elif aReqValue < 0.0:
-                  #   stock_weight = interp(stop_distance, [6.0, 10.0, 18.0, 25.0, 32.0], [1.0, 1.0, 1.0, 1.0, 1.0])
-                  #   apply_accel = apply_accel * (1.0 - stock_weight) + aReqValue * stock_weight                  
                   else:
-                    stock_weight = 0.0
-                    apply_accel = apply_accel * (1.0 - stock_weight) + aReqValue * stock_weight
+                    apply_accel = aReqValue
 
-                  str_log2 = 'LPSource.stop: aReqValue={:02.3f} apply_accel={:02.3f}  stopLine={:03.0f} MPH={:02.0f} set_speed={:02.0f}'.format(
-                              aReqValue, apply_accel, stop_distance, CS.out.vEgo*CV.MS_TO_MPH, set_speed )
-                  self.log.add( '{}'.format( str_log2 ) )
+                  str_log = '{:03.0f}, {:03.2f}, {:03.2f}, {:02.0f}, {:02.0f}'.format(
+                            stop_distance, aReqValue, apply_accel, CS.out.vEgo*CV.MS_TO_MPH, set_speed )
+                  self.log.add( '{}'.format( str_log ) )
+
+                  # str_log2 = 'LPSource.stop: aReqValue={:02.3f} apply_accel={:02.3f}  stopLine={:03.0f} MPH={:02.0f} set_speed={:02.0f}'.format(
+                  #             aReqValue, apply_accel, stop_distance, CS.out.vEgo*CV.MS_TO_MPH, set_speed )
+                  # self.log.add( '{}'.format( str_log2 ) )
 
               if stopping:
                 self.stopped = True
