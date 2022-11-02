6cfa4220549fe565dde9afbc82ea3d2de163052e
sunnyc-op <sunny68@outlook.com>
2022-11-02 08:25:35 -0700

    Stopline Test Version.

--- selfdrive/ntune.py
+++ selfdrive/ntune.py
@@ -272,13 +272,13 @@ class nTune():
     if self.checkValue("StopAtStopSign", 0., 1., 1.):
       updated = True
 
-    if self.checkValue("StoppingDist", 5.0, 25., 20.):
+    if self.checkValue("StoppingDist", 18.0, 30., 20.):
       updated = True
 
-    if self.checkValue("STOP_LINE_OFFSET", 0.7, 1.2, 0.95):
+    if self.checkValue("STOP_LINE_OFFSET", 0.7, 1.2, 0.90):
       updated = True
 
-    if self.checkValue("STOP_LINE_X_OFFSET", -3.0, 3.0, 1.0):
+    if self.checkValue("STOP_LINE_X_OFFSET", -3.0, 3.0, 0.0):
       updated = True
 
     return updated
