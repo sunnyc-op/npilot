38cc639a933cd312c8999f632e67a130581d71fd
sunnyc-op <95874127+sunnyc-op@users.noreply.github.com>
2022-11-11 15:00:51 -0800

    Update latcontrol_torque.py

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -70,8 +70,8 @@ class LatControlTorque(LatControl):
 
       if isLowSpeed:
         #low_speed_factor = interp(CS.vEgo, [0, 15], [500, 0]) # comma 1st
-        #low_speed_factor = interp(CS.vEgo, [0, 10, 20], [500, 500, 200]) # comma 2nd
-        low_speed_factor = interp(CS.vEgo, LOW_SPEED_X, LOW_SPEED_Y)**2 #
+        low_speed_factor = interp(CS.vEgo, [0, 10, 20], [500, 500, 200]) # comma 2nd
+        #low_speed_factor = interp(CS.vEgo, LOW_SPEED_X, LOW_SPEED_Y)**2 #
       else:
         low_speed_factor = interp(CS.vEgo, [0, 5], [300, 0])
 
