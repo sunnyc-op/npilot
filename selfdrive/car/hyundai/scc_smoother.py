0220df2e59a6c3432f9445e337717dc6b537584c
sunnyc-op <95874127+sunnyc-op@users.noreply.github.com>
2022-12-01 13:48:48 -0800

    Update scc_smoother.py

--- selfdrive/car/hyundai/scc_smoother.py
+++ selfdrive/car/hyundai/scc_smoother.py
@@ -173,13 +173,7 @@ class SccSmoother:
           self.slowing_down_sound_alert = True
           self.slowing_down = True
 
-        ascc_enabled = CS.acc_mode and CS.cruiseState_enabled \
-                   and 1 < CS.cruiseState_speed < 255 and not CS.brake_pressed
-        
-        if ascc_enabled:
-          self.slowing_down_alert = True
-        else:
-          self.slowing_down_alert = False
+        self.slowing_down_alert = True
 
       else:
         self.slowing_down_alert = False
